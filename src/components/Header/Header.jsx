0c3ea4ac79ad040b4c220f9d6a33a42ac09070fa
AKaramifar <a.karamifar@gmail.com>
2020-09-04 09:44:26 +0100

    update-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ selectedNav, selectNav }) => {
     <div className="header animate__animated animate__fadeInDown">
       <div className="header-logo">
         <p>
-          &#123;&nbsp;<strong>/* AFSHIN KARAMIFAR */</strong>&nbsp;&#125;
+          &#123;&nbsp;<strong>&#x0002F;* AFSHIN KARAMIFAR *&#x0002F;</strong>&nbsp;&#125;
         </p>
       </div>
       <div className="header-navbar">
@@ -17,7 +17,7 @@ const Header = ({ selectedNav, selectNav }) => {
           to="intro"
           offset={0}
           containerId="body"
-          smooth={true}          
+          smooth={true}
           duration={1000}
         >
           <hr />
